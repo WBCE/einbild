ef787dce51bb6c16c886cffc9838adeff87b0977
instantflorian <info@meerwinck.com>
2020-05-28 08:33:07 +0200

    Update info.php

--- info.php
+++ info.php
@@ -14,8 +14,12 @@ if(count(get_included_files()) ==1){$z="HTTP/1.0 404 Not Found";header($z);die($
 $module_directory = 'einbild';
 $module_name = 'einbild';
 $module_function = 'page';
-$module_version = '0.2';
+$module_version = '0.3';
 $module_platform = '1.4.0';
 $module_author = 'florian';
 $module_license = 'WTFPL';
 $module_description = 'Place an image somewhere. ';
+
+/*
+0.3 2020/05/27
+! Fix for Fatal Error when used with other wysiwyg2 based modules on the same page (thx to Bernd)
